d96c8b041898475561c11d3081252665b71debab
Rafia Khandaker <rafiakhandaker@gmail.com>
2022-07-21 20:41:15 -0400

    commit tree a6cce0221ac6e324f0d0358bab5a5361adc1aa27

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]; 
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name){
+    const newCatsEnd = [...cats, "Broom"];
+    return newCatsEnd;
+}
+
+function prependCat(name) {
+    const newCatsBeg = ["Arnold", ...cats];
+    return newCatsBeg;
+}
+
+function removeLastCat(){
+    const ByeGarfield = cats.slice(0,2);
+    return ByeGarfield;
+}
+
+function removeFirstCat(){
+    const ByeMilo = cats.slice(1);
+    return ByeMilo;
+}
